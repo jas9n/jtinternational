4bbc66798a6e68b20aa1ada4b7c9ff4068c414ff
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2024-07-19 14:42:36 -0400

    pinia, gsap, tailwind

--- project/nuxt.config.ts
+++ project/nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'JagTrim International'
+    }
+  },
+  modules: ['@pinia/nuxt'],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
