c624773e2f3ee9865a7e5701f0fba04a7877f19e
jasonchen3805 <90630687+jasonc3805@users.noreply.github.com>
2024-07-22 13:22:48 -0400

    pictures

--- project/components/LandingView.vue
+++ project/components/LandingView.vue
@@ -4,11 +4,11 @@ import gsap from 'gsap'
 
 <template>
     <div class="w-screen h-screen flex justify-center items-center z-10">
-      <img id="landing-img" src="/images/photos/landing.svg" alt="" class="h-screen w-full object-cover absolute -z-10 top-0 ">
+      <img id="landing-img" src="/images/photos/main.jpg" alt="" class="h-screen w-full object-cover absolute -z-10 top-0 ">
       <img id="monogram1" src="/images/logos/monogram-j.svg" class="hidden absolute h-screen w-full object-cover top-0 md:block" alt="">
       <img id="monogram2" src="/images/logos/monogram-t.svg" class="hidden absolute h-screen w-full object-cover top-0 md:block" alt="">
       <div class="flex justify-center items-start absolute right-12 sm:right-20 md:right-24 xl:right-40">
-        <h1 class="flex flex-col items-end space-y-3 pb-2 font-serif text-4xl md:text-6xl lg:text-7xl">
+        <h1 class="flex flex-col items-end space-y-3 pb-2 font-serif text-zinc-800 text-4xl md:text-6xl lg:text-7xl">
           <span id="title">Design by passion.</span>
           <span id="title">Craft with precision.</span> 
         </h1>
@@ -20,10 +20,10 @@ import gsap from 'gsap'
 export default {
     name: 'LandingView',
     mounted() {
-        const inital = gsap.timeline()
-        inital.to('#monogram1', {x: '-100%',  opacity: 0.1, duration: 2}, 0.3)
-        inital.to('#monogram2', {x: '100%', opacity: 0.1, duration: 2}, 0.3)
-        inital.from('#title', {y: -40, opacity: 0, duration: 1, stagger: 0.4}, 1.5)
+        gsap.from('#landing-img', {opacity: 0, duration: 2}, 0.3)
+        gsap.to('#monogram1', {x: '-100%',  opacity: 0.1, duration: 2}, 0.3)
+        gsap.to('#monogram2', {x: '100%', opacity: 0.1, duration: 2}, 0.3)
+        gsap.from('#title', {y: -40, opacity: 0, duration: 1, stagger: 0.4}, 1.5)
     }
 }
 </script>
--- project/components/PhotoGallery.vue
+++ project/components/PhotoGallery.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex justify-center items-center w-full md:h-screen h-[50vh]">
         <div class="group flex justify-center items-center relative w-[90vw] lg:w-[80vw]">
-            <img src="https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="item absolute transition duration-250"/>
-            <img src="https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="item absolute transition duration-250"/>
-            <img src="https://images.unsplash.com/photo-1500673922987-e212871fec22?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="item absolute transition duration-250"/>
-            <img src="https://images.unsplash.com/photo-1458668383970-8ddd3927deed?q=80&w=2967&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="item absolute transition duration-250"/>
+            <img src="/images/photos/materials.jpg" class="item absolute transition duration-250"/>
+            <img src="/images/photos/shelf1.jpg" class="item absolute transition duration-250"/>
+            <img src="/images/photos/ceo.jpg" class="item absolute transition duration-250"/>
+            <img src="/images/photos/office.jpg" class="item absolute transition duration-250"/>
             <p id="prev" class="opacity-60 md:opacity-0 cursor-pointer text-white absolute left-0 bg-black text-2xl p-3 rounded group-hover:opacity-60 transition duration-250" @click="prev()">&#10094;</p>
             <p id="next" class="opacity-60 md:opacity-0  cursor-pointer text-white absolute right-0 bg-black text-2xl p-3 rounded group-hover:opacity-60 transition duration-250" @click="next()">&#10095;</p>
         </div>
